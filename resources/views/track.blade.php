40c4a70b9371999cd93dafdcfbde7226e4c583a6
杨圆建 <624508914@qq.com>
2018-03-20 13:46:41 +0800

    更新代码

--- resources/views/track.blade.php
+++ resources/views/track.blade.php
@@ -29,16 +29,16 @@
     function showError(error){
         switch(error.code) {
             case error.PERMISSION_DENIED:
-                alert("定位失败,用户拒绝请求地理定位");
+                // alert("定位失败,用户拒绝请求地理定位");
                 break;
             case error.POSITION_UNAVAILABLE:
-                alert("定位失败,位置信息是不可用");
+                // alert("定位失败,位置信息是不可用");
                 break;
             case error.TIMEOUT:
-                alert("定位失败,请求获取用户位置超时");
+                // alert("定位失败,请求获取用户位置超时");
                 break;
             case error.UNKNOWN_ERROR:
-                alert("定位失败,定位系统失效");
+                // alert("定位失败,定位系统失效");
                 break;
         }
     }
